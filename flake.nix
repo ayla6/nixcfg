a2e45da76ba3ea851521c0abccbd758736a83c86
ayla <ayla-git.barcode041@silomails.com>
2025-08-12 18:26:00 -0300

    dumbass

--- flake.nix
+++ flake.nix
@@ -63,6 +63,12 @@
       "https://chaotic-nyx.cachix.org/"
       "https://nix-community.cachix.org"
     ];
+
+    extra-trusted-public-keys = [
+      "ayla6.cachix.org-1:40BzoflmIK8MovQ5zewLsWlDNWQh7Gdtu2i220h1YmE="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   outputs = inputs @ {flake-parts, ...}:
