cbb879842f7544746f20345c93f798681bcbcb70
ayla <ayla-git.barcode041@silomails.com>
2025-08-13 22:56:45 -0300

    ok now i can go back on this wait a minute im an idiot i was doing it the opposite way

--- modules/flake/devShells.nix
+++ modules/flake/devShells.nix
@@ -7,34 +7,28 @@ _: {
     self',
     ...
   }: {
-    devShells = {
-      morgana =
-        pkgs.mkShell {
-        };
+    devShells.default = pkgs.mkShell {
+      packages =
+        (with pkgs; [
+          (lib.hiPrio uutils-coreutils-noprefix)
+          git
+          nh
+        ])
+        # ++ lib.attrValues config.treefmt.build.programs
+        ++ [
+          inputs'.agenix.packages.default
+          inputs'.disko.packages.disko-install
+          self'.packages.gen-files
+        ];
 
-      default = pkgs.mkShell {
-        packages =
-          (with pkgs; [
-            (lib.hiPrio uutils-coreutils-noprefix)
-            git
-            nh
-          ])
-          # ++ lib.attrValues config.treefmt.build.programs
-          ++ [
-            inputs'.agenix.packages.default
-            inputs'.disko.packages.disko-install
-            self'.packages.gen-files
-          ];
-
-        shellHook = ''
-          echo "Installing pre-commit hooks..."
-          ${config.pre-commit.installationScript}
-          echo "Generating files..."
-          ${lib.getExe self'.packages.gen-files}
-          export FLAKE="." NH_FLAKE="."
-          echo "👋 Welcome to the nixcfg devShell!"
-        '';
-      };
+      shellHook = ''
+        echo "Installing pre-commit hooks..."
+        ${config.pre-commit.installationScript}
+        echo "Generating files..."
+        ${lib.getExe self'.packages.gen-files}
+        export FLAKE="." NH_FLAKE="."
+        echo "👋 Welcome to the nixcfg devShell!"
+      '';
     };
   };
 }
