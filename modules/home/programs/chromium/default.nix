6c7d23909c3e175e6903cda2da647eac4041d9ca
ayla <ayla-git.barcode041@silomails.com>
2025-08-16 04:26:44 -0300

    firefox

--- modules/home/programs/chromium/default.nix
+++ modules/home/programs/chromium/default.nix
@@ -16,14 +16,25 @@
         {id = "clngdbkpkpeebahjckkjfobafhncgmne";} # stylus
         {id = "oboonakemofpalcgghocfoadofidjkkk";} # keepassxc
         {id = "jinjaccalgkegednnccohejagnlnfdag";} # violentmonkey
+        {id = "nngceckbapebfimnlniiiahkandclblb";} # bitwarden
 
         rec {
-          id = "libredirectlibredirectlibredirec"; # libredirect
+          id = "oladmjdebphlnjjcnomfhhbfdldiimaf"; # libredirect
           version = "3.2.0";
 
           crxPath = pkgs.fetchurl {
             url = "https://github.com/libredirect/browser_extension/releases/download/v${version}/libredirect-${version}.crx";
             sha256 = "sha256-Neja0pJ7rMV+riINeMcWxU2SzZ+ZETp6bV1MaYLHz1Y=";
+          };
+        }
+
+        rec {
+          id = "lkbebcjgcmobigpeffafkodonchffocl"; # bypass-paywalls-clean
+          version = "4.1.8.0";
+
+          crxPath = pkgs.fetchurl {
+            url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass-paywalls-chrome-clean-${version}.crx";
+            sha256 = "sha256-BRpwrV8AN1eOG2IXfk24gyEd8OzwK1BJqDdoxlgX8o4=";
           };
         }
       ];
