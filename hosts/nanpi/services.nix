1e6a749becf764710db3b4a4a9dbe13ae4027311
ayla <ayla-git.barcode041@silomails.com>
2025-08-19 05:00:32 -0300

    scrl c ctrl v

--- hosts/nanpi/services.nix
+++ hosts/nanpi/services.nix
@@ -70,6 +70,54 @@ in {
           reverse_proxy ${config.mySnippets.tailnet.networkMap.webdav.hostName}:${toString config.mySnippets.tailnet.networkMap.webdav.port}
         '';
       };
+
+      "${config.mySnippets.tailnet.networkMap.bazarr.vHost}" = {
+        extraConfig = ''
+          bind tailscale/bazarr
+          encode zstd gzip
+          reverse_proxy ${config.mySnippets.tailnet.networkMap.bazarr.hostName}:${toString config.mySnippets.tailnet.networkMap.bazarr.port}
+        '';
+      };
+
+      #"${config.mySnippets.tailnet.networkMap.lidarr.vHost}" = {
+      #  extraConfig = ''
+      #    bind tailscale/lidarr
+      #    encode zstd gzip
+      #    reverse_proxy ${config.mySnippets.tailnet.networkMap.lidarr.hostName}:${toString config.mySnippets.tailnet.networkMap.lidarr.port}
+      #  '';
+      #};
+
+      "${config.mySnippets.tailnet.networkMap.prowlarr.vHost}" = {
+        extraConfig = ''
+          bind tailscale/prowlarr
+          encode zstd gzip
+          reverse_proxy ${config.mySnippets.tailnet.networkMap.prowlarr.hostName}:${toString config.mySnippets.tailnet.networkMap.prowlarr.port}
+        '';
+      };
+
+      "${config.mySnippets.tailnet.networkMap.radarr.vHost}" = {
+        extraConfig = ''
+          bind tailscale/radarr
+          encode zstd gzip
+          reverse_proxy ${config.mySnippets.tailnet.networkMap.radarr.hostName}:${toString config.mySnippets.tailnet.networkMap.radarr.port}
+        '';
+      };
+
+      "${config.mySnippets.tailnet.networkMap.sonarr.vHost}" = {
+        extraConfig = ''
+          bind tailscale/sonarr
+          encode zstd gzip
+          reverse_proxy ${config.mySnippets.tailnet.networkMap.sonarr.hostName}:${toString config.mySnippets.tailnet.networkMap.sonarr.port}
+        '';
+      };
+
+      "${config.mySnippets.tailnet.networkMap.glance.vHost}" = {
+        extraConfig = ''
+          bind tailscale/glance
+          encode zstd gzip
+          reverse_proxy ${config.mySnippets.tailnet.networkMap.glance.hostName}:${toString config.mySnippets.tailnet.networkMap.glance.port}
+        '';
+      };
     };
 
     # it's failing to build because it can't download some stuff
@@ -120,30 +168,151 @@ in {
       };
     };
 
-    webdav-server-rs = {
+    glance = {
       enable = true;
+      openFirewall = true;
+
       settings = {
-        server.listen = ["0.0.0.0:${toString config.mySnippets.tailnet.networkMap.webdav.port}" "[::]:${toString config.mySnippets.tailnet.networkMap.webdav.port}"];
-        accounts = {
-          auth-type = "htpasswd.default";
-          acct-type = "unix";
-        };
-        htpasswd.default = {
-          htpasswd = pkgs.writeText "htpasswd" ''
-            ayla:$2y$05$LD.VqJF.yVGsp.C3L6IJFO0SvYTeCKbGoGn70ZQaht4gxyEq2XbCS
-          '';
-        };
-        location = [
+        pages = [
           {
-            route = ["/*path"];
-            directory = "${dataDirectory}/webdav";
-            handler = "filesystem";
-            methods = ["webdav-rw"];
-            autoindex = true;
-            auth = "true";
+            name = config.mySnippets.tailnet.networkMap.glance.vHost;
+            width = "slim";
+            hide-desktop-navigation = true;
+            center-vertically = true;
+            columns = [
+              {
+                size = "full";
+                widgets = [
+                  {
+                    type = "search";
+                    autofocus = true;
+                  }
+                  {
+                    type = "monitor";
+                    cache = "1m";
+                    title = "Public Services";
+
+                    sites = [
+                      {
+                        title = "Forgejo";
+                        url = "https://${config.mySnippets.aylac-top.networkMap.forgejo.vHost}/";
+                        check-url = "http://${config.mySnippets.aylac-top.networkMap.forgejo.hostName}:${toString config.mySnippets.aylac-top.networkMap.forgejo.port}/";
+                        icon = "di:forgejo";
+                      }
+                      {
+                        title = "PDS";
+                        url = "https://${config.mySnippets.aylac-top.networkMap.pds.vHost}/";
+                        check-url = "http://${config.mySnippets.aylac-top.networkMap.pds.hostName}:${toString config.mySnippets.aylac-top.networkMap.pds.port}/";
+                        icon = "di:bluesky";
+                      }
+                      {
+                        title = "Vaultwarden";
+                        url = "https://${config.mySnippets.aylac-top.networkMap.vaultwarden.vHost}/";
+                        check-url = "http://${config.mySnippets.aylac-top.networkMap.vaultwarden.hostName}:${toString config.mySnippets.aylac-top.networkMap.vaultwarden.port}/";
+                        icon = "di:vaultwarden";
+                      }
+                    ];
+                  }
+                  {
+                    type = "monitor";
+                    cache = "1m";
+                    title = "Private Services";
+
+                    sites = [
+                      {
+                        title = "Jellyfin";
+                        url = "https://${config.mySnippets.tailnet.networkMap.jellyfin.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.jellyfin.hostName}:${toString config.mySnippets.tailnet.networkMap.jellyfin.port}/web/index.html";
+                        icon = "di:jellyfin";
+                      }
+                      {
+                        title = "Sonarr";
+                        url = "https://${config.mySnippets.tailnet.networkMap.sonarr.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.sonarr.hostName}:${toString config.mySnippets.tailnet.networkMap.sonarr.port}/";
+                        icon = "di:sonarr";
+                      }
+                      {
+                        title = "Radarr";
+                        url = "https://${config.mySnippets.tailnet.networkMap.radarr.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.radarr.hostName}:${toString config.mySnippets.tailnet.networkMap.radarr.port}/";
+                        icon = "di:radarr";
+                      }
+                      #{
+                      #  title = "Lidarr";
+                      #  url = "https://${config.mySnippets.tailnet.networkMap.lidarr.vHost}/";
+                      #  check-url = "http://${config.mySnippets.tailnet.networkMap.lidarr.hostName}:${toString config.mySnippets.tailnet.networkMap.lidarr.port}/";
+                      #  icon = "di:lidarr";
+                      #}
+                      {
+                        title = "Prowlarr";
+                        url = "https://${config.mySnippets.tailnet.networkMap.prowlarr.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.prowlarr.hostName}:${toString config.mySnippets.tailnet.networkMap.prowlarr.port}/";
+                        icon = "di:prowlarr";
+                      }
+                      {
+                        title = "Bazarr";
+                        url = "https://${config.mySnippets.tailnet.networkMap.bazarr.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.bazarr.hostName}:${toString config.mySnippets.tailnet.networkMap.bazarr.port}/";
+                        icon = "di:bazarr";
+                      }
+                      {
+                        title = "qBittorrent";
+                        url = "https://${config.mySnippets.tailnet.networkMap.qbittorrent.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.qbittorrent.hostName}:${toString config.mySnippets.tailnet.networkMap.qbittorrent.port}/";
+                        icon = "di:qbittorrent";
+                        alt-status-codes = [401];
+                      }
+                      {
+                        title = "Uptime Kuma";
+                        url = "https://${config.mySnippets.tailnet.networkMap.uptime-kuma.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.uptime-kuma.hostName}:${toString config.mySnippets.tailnet.networkMap.uptime-kuma.port}/";
+                        icon = "di:uptime-kuma";
+                      }
+                      {
+                        title = "Radicale";
+                        url = "https://${config.mySnippets.tailnet.networkMap.radicale.vHost}/";
+                        check-url = "http://${config.mySnippets.tailnet.networkMap.radicale.hostName}:${toString config.mySnippets.tailnet.networkMap.radicale.port}/";
+                        icon = "di:radicale";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
           }
         ];
+
+        server = {
+          host = "0.0.0.0";
+          inherit (config.mySnippets.tailnet.networkMap.glance) port;
+        };
       };
+    };
+  };
+
+  services.webdav-server-rs = {
+    enable = true;
+    settings = {
+      server.listen = ["0.0.0.0:${toString config.mySnippets.tailnet.networkMap.webdav.port}" "[::]:${toString config.mySnippets.tailnet.networkMap.webdav.port}"];
+      accounts = {
+        auth-type = "htpasswd.default";
+        acct-type = "unix";
+      };
+      htpasswd.default = {
+        htpasswd = pkgs.writeText "htpasswd" ''
+          ayla:$2y$05$LD.VqJF.yVGsp.C3L6IJFO0SvYTeCKbGoGn70ZQaht4gxyEq2XbCS
+        '';
+      };
+      location = [
+        {
+          route = ["/*path"];
+          directory = "${dataDirectory}/webdav";
+          handler = "filesystem";
+          methods = ["webdav-rw"];
+          autoindex = true;
+          auth = "true";
+        }
+      ];
     };
   };
 
