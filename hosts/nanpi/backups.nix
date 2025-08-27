263d303434e243e1100435d27f2e56c9d7f0b177
ayla <ayla-git.barcode041@silomails.com>
2025-08-27 00:18:43 -0300

    not sure if the config for the disks is perfect but maybe it works. now we have an external disk at nanpi!

---
+++ hosts/nanpi/backups.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  localRepo = "/external1/backups/${config.networking.hostName}";
+  mirrorRepo = "/external1/mirrorBackups";
+in {
+  services.restic.backups = {
+    pictures =
+      config.mySnippets.restic
+      // {
+        paths = ["/data/DCIM" "/data/Pictures"];
+        repository = "${localRepo}/pictures";
+      };
+  };
+
+  systemd.services.rclone-mirror = {
+    description = "Sync backups from cloud to local mirror";
+    path = with pkgs; [rclone];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    script = ''
+      set -e
+      rclone --config ${config.age.secrets.rclone.path} sync "a_gdrive:/backups" "${mirrorRepo}"
+    '';
+  };
+
+  systemd.timers.rclone-mirror = {
+    description = "Run rclone mirror sync daily at 3am";
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+      RandomizedDelaySec = "10m";
+    };
+  };
+}
--- modules/snippets/restic/default.nix
+++ modules/snippets/restic/default.nix
@@ -32,7 +32,7 @@
           #OnCalendar = "*-*-* 02,14:00:00";
           #OnCalendar = "*-*-* 03:14:00";
           Persistent = true;
-          RandomizedDelaySec = "10";
+          RandomizedDelaySec = "10m";
         };
       };
     };
