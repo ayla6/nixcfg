e197f6cf7f7f537fb3fd1e6bf0a9ef4ce67b472d
ayla <ayla-git.barcode041@silomails.com>
2025-10-28 19:03:38 -0300

    kill github

--- modules/nixos/programs/nix/default.nix
+++ modules/nixos/programs/nix/default.nix
@@ -2,8 +2,11 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
-}: {
+}: let
+  flakeInputs = lib.filterAttrs (name: value: (lib.isType "flake" value) && (name != "self")) inputs;
+in {
   options.myNixOS.programs.nix.enable = lib.mkEnableOption "sane nix configuration";
 
   config = lib.mkIf config.myNixOS.programs.nix.enable {
@@ -24,6 +27,15 @@
         persistent = true;
         randomizedDelaySec = "60min";
       };
+
+      # https://github.com/isabelroses/dotfiles/blob/main/modules/base/nix/nix.nix#L34-L38
+      # pin the registry to avoid downloading and evaluating a new nixpkgs version everytime
+      registry =
+        (lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs)
+        // {
+          # https://github.com/NixOS/nixpkgs/pull/388090
+          nixpkgs = lib.mkForce {flake = inputs.nixpkgs;};
+        };
 
       inherit (config.mySnippets.nix) settings;
     };
